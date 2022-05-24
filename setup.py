cbbf56688604459f2a4ff103f03bd3e9d1576215
Rick Getz <rick@presagesecurity.com>
2022-05-24 14:55:47 -0400

    Setup and ignore added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+requirements = ['requests']
+
+
+setup(
+    name='Presage Technologies',
+    version='1.0.0',
+    packages=['presage_technologies'],
+    package_dir={'presage_technologies': 'presage_technologies'},
+    install_requires=requirements,
+    zip_safe=False
+)
