21c342c132ead21919a15b0803f4eecdef24991e
Rick Getz <rick@presagesecurity.com>
2022-05-24 14:43:03 -0400

    Initial SDK working. Need to add setup file and implement preprocessing. May need changes on the API to support json vs video uploads.

---
+++ presage_technologies/physiology.py
@@ -0,0 +1,87 @@
+import requests
+import time
+import logging
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s [%(process)d] [%(funcName)20s] %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+class PresagePhysiology:
+    def __init__(self, api_key, base_api_url="https://physiology.presagetech.com"):
+        self.api_key = api_key
+        self.base_api_url = base_api_url
+        self.s3_client = boto3.client('s3')
+    def preprocess_video(self, video):
+        logging.warning("Not yet implemented")
+        pass
+    def retrieve_result(self, id, timeout=1800):
+        """Short summary.
+
+        Parameters
+        ----------
+        id : str
+            Id of the uploaded video that was retireved during the video or json payload upload step.
+        timeout : int
+            Default: 1800. How long to try, in seconds, until to give up.
+
+        Returns
+        -------
+        dict
+            JSON payload returned from the API for the id you chose. If no value is returned before timeout a None value will be returned.
+
+        """
+        stop_at = time.time()+timeout
+
+        while True:
+            if timeout and time.time()>=stop_at:
+                logging.warning("Timeout triggered for id", id,"please try again later.")
+                return None
+            response = requests.post(
+                self.base_api_url+ "/retrieve-data",
+                headers={"x-api-key": self.api_key},
+                json={"id": id},
+            )
+            if response.status_code == 200:
+                return response.json()
+            elif response.status_code == 201:
+                time.sleep(5)
+            else:
+                time.sleep(60)
+        return None
+
+    def queue_processing_hr_rr(self, video_path, preprocess=False):
+        """Using the Presage Physiology API, get the Heart Rate and Resporation Rate
+        of a subject within a video.
+
+        Returns
+        -------
+        str
+            Id for the video uploaded that can be used to later retrieveresults with the retrieve_result function.
+        """
+        response = requests.get(self.base_api_url+ "/upload-url", headers={"x-api-key": self.api_key})
+        link = response.json()["url"]
+        vid_id = response.json()["id"]
+        if preprocess:
+            pass
+        else:
+            #TODO: implement wait on server overload status
+            # Upload file to S3 using presigned URL
+            with open(video_path, "rb") as f:
+                files = {"file": (vid_id, f)}
+                r = requests.post(link["url"], data=link["fields"], files=files)
+                logging.info("Video uploaded successfully and is now processing.")
+        return vid_id
+
+
+if __name__ == '__main__':
+    import boto3
+    ssm = boto3.client('ssm')
+    api_key = ssm.get_parameter(Name='/physiology/rick/test/api_key', WithDecryption=True)['Parameter']['Value']
+
+
+    physio = PresagePhysiology(api_key, base_api_url="https://test.physiology.presagetech.com")
+    vid_id = physio.queue_processing_hr_rr("/Users/rick/Desktop/rotating90bpm.mov")
+    print("Have video id", vid_id)
+    # time.sleep(120)
+    final_data = physio.retrieve_result(vid_id)
+    print(final_data)
