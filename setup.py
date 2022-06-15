c02ec184bc0d22893e6ed3ee335bbc80d37f7fb0
Rick Getz <rick@presagesecurity.com>
2022-06-15 10:34:50 -0400

    Updating setup file for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,20 @@ setup(
     name='Presage Technologies',
     version='1.0.1',
     packages=['presage_technologies'],
+    author="Presage Technologies",
+    author_email="support@presagetech.com",
+    description="A Python client to interface with Presage Technologies' API services.",
     package_dir={'presage_technologies': 'presage_technologies'},
     install_requires=requirements,
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    url="https://github.com/Presage-Security/presage_technologies",
+    project_urls={
+        "Bug Tracker": "https://github.com/Presage-Security/presage_technologies/issues",
+    },
 )
