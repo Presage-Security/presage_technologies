54e94d3fd1f1630060c77b6bcca8235e1947e482
andressbryce <andressbryce@gmail.com>
2022-06-21 13:20:33 -0400

    Trying to add semantic release

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 requirements = ['requests']
 
+__version__ = '1.0.0'
 
 setup(
     name='Presage Technologies',
-    version='1.0.0',
+    version=__version__,
     packages=['presage_technologies'],
     package_dir={'presage_technologies': 'presage_technologies'},
     install_requires=requirements,
