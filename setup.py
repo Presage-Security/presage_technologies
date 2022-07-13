e3cb704d2259acdf40e60481f619f55ff3dceea9
Rick Getz <rick@presagesecurity.com>
2022-07-13 13:53:50 -0400

    Correct version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 requirements = ['requests']
 
-__version__ = '1.2.0'
+__version__ = '1.1.0'
 
 setup(
     name='Presage Technologies',
