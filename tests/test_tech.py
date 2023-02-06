08b87c7a2d7528edb83aea189cc1923f5f09daa1
Rick Getz <rick@presagetech.com>
2023-02-06 16:26:07 -0500

    fix: update so2 to all flag for retrieving all available vital types

--- tests/test_tech.py
+++ tests/test_tech.py
@@ -4,9 +4,9 @@ import sys
 sys.path.append('..')
 from presage_technologies import Physiology
 
-physio = Physiology("", base_api_url="https://api.test.physiology.presagetech.com")
-# vid_id = physio.queue_processing_hr_rr("/Users/rick/Desktop/test_videos/vid_aya_15x30bpm.avi")
-vid_id = physio.queue_processing_all("/Users/rick/Desktop/mobile_example.mp4", preprocess=True)
+physio = Physiology("EhPM5zB8vM7UM9C0A0uzV1jyfDjLaNN4YOstVTR7", base_api_url="https://api.test.physiology.presagetech.com")
+vid_id = physio.queue_processing_hr_rr("/Users/rick/Desktop/mobile_example.mp4", preprocess=True)
+# vid_id = physio.queue_processing_all("/Users/rick/Desktop/mobile_example.mp4", preprocess=True)
 print("Have video id", vid_id)
 time.sleep(60)
 final_data = physio.retrieve_result(vid_id)
